e9ec3f416e86e16a46b3fbf1e9eb8493af794e05
Gilelberto <gil_contr02@hotmail.com>
2023-05-26 20:59:04 -0600

    Update infoGil.js

--- infoGil.js
+++ infoGil.js
@@ -1,1 +1,13 @@
-
+{
+  "name": "Gilberto Contreras Conn",
+  "phone": "6142890579",
+  "email": "a348503@uach.mx",
+  "introduction": "Computer science student",
+  "objective": "I am looking for a job where I can expand my abilities while working on world-class software",
+  "experience": "GitHub",
+  "academic_preparation": "Bachelor's degree in Computer Science Engineer at UACH (Universidad Autónoma de Chihuahua), 2020 - Present day; Clinical Laboratory Assistant at COBACH 4 (Colegio de Bachilleres del estado de Chihuahua), 2017 - 2020",
+  "languages": "Spanish (Native), English (B1)",
+  "knowledge": "Python core and some libraries like Tkinter, matplotlib, etc.; JavaScript core and NodeJS (NodeJS at a basic level); Git; Docker (basic); Other languages like C#, C/C++ and Java; SQL databases",
+  "skills": "Teamwork, Ease to explain, Creativity, Problem-solving",
+  "about_me": "I'm a Mexican student who has had a passion for technology and science since I was a kid. I enjoy exploring how things work behind the screens of my devices and have always been intrigued by the magic of images appearing on a piece of glass, silicon, bakelite, aluminum, and copper. Outside of the IT field, I also have a love for music and enjoy working out at the gym."
+}
